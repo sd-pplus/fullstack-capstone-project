bc9115562ae19c93c736504255d0503686551923
sd-pplus <sandep.dinesh@protonmail.com>
2026-08-19 12:00:04 -0400

    search route

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,6 +7,8 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+
 
 
 const app = express();
@@ -27,7 +29,7 @@ app.use(express.json());
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 
 const pinoHttp = require('pino-http');
