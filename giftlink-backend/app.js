5e65bc953e65510a127872c8e58019fd9b87b239
sd-pplus <sandep.dinesh@protonmail.com>
2026-08-19 12:38:27 -0400

    sentiment api

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -6,10 +6,6 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-const giftRoutes = require('./routes/giftRoutes');
-const searchRoutes = require('./routes/searchRoutes');
-
-
 
 const app = express();
 app.use("*",cors());
@@ -26,11 +22,11 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-app.use('/api/gifts', giftRoutes);
+const giftRoutes = require('./routes/giftRoutes');
+
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-app.use('/api/search', searchRoutes);
-
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -39,11 +35,10 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
