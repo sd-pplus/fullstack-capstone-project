9834e7e2672ccc7fded549e2a34fcac48d7fe017
sd-pplus <sandep.dinesh@protonmail.com>
2026-08-20 15:49:48 -0400

    fix lint errors

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -96,7 +96,7 @@ return (
                             <div className="no-image-available-large">No Image Available</div>
                         )}
                     </div>
-                    // Task 6: Display gift details
+                    {/* Task 6: Display gift details */}
                     	<p><strong>Category:</strong> 
 				{/* insert code here  */}
                 {gift.category}
@@ -121,7 +121,7 @@ return (
             </div>
             <div className="comments-section mt-4">
                 <h3 className="mb-3">Comments</h3>
-				// Task 7: Render comments section by using the map function to go through all the comments
+				{/* Task 7: Render comments section by using the map function to go through all the comments */}
 				{comments.map((comment, index) => (
                     <div key={index} className="card mb-3">
                         <div className="card-body">
