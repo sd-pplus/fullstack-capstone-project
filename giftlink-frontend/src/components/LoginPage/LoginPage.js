860c8917b9d58dca587a47eb90efe07aca76289a
sd-pplus <sandep.dinesh@protonmail.com>
2026-08-20 15:12:57 -0400

    run time updates

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './LoginPage.css';
 import { urlConfig } from '../../config';
 import { useAppContext } from '../../context/AuthContext';
