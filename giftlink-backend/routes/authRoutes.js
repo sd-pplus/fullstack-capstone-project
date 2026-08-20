7bbc07c79c7abe3d323f287e7d1b39936315eed9
sd-pplus <sandep.dinesh@protonmail.com>
2026-08-20 13:27:26 -0400

    Login API

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');
 //Step 1 - Task 3: Create a Pino logger instance
-const logger = pino(); 
+const logger = pino();
 dotenv.config();
 
 //Step 1 - Task 4: Create JWT secret
@@ -18,19 +18,19 @@ const JWT_SECRET = process.env.JWT_SECRET;
 router.post('/register', async (req, res) => {
     try {
         // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
-		 const db = await connectToDatabase();
+        const db = await connectToDatabase();
 
         // Task 2: Access MongoDB collection
-		const collection = db.collection("users");
+        const collection = db.collection("users");
 
-		//Task 3: Check for existing email
-		const existingEmail = await collection.findOne({ email: req.body.email });
+        //Task 3: Check for existing email
+        const existingEmail = await collection.findOne({ email: req.body.email });
 
-		const salt = await bcryptjs.genSalt(10);
+        const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
-		const email = req.body.email;
+        const email = req.body.email;
 
-		// {{insert code here}} //Task 4: Save user details in database
+        // {{insert code here}} //Task 4: Save user details in database
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -39,8 +39,8 @@ router.post('/register', async (req, res) => {
             createdAt: new Date(),
         });
 
-		 // {{insert code here}} //Task 5: Create JWT authentication with user._id as payload
-         const payload = {
+        // {{insert code here}} //Task 5: Create JWT authentication with user._id as payload
+        const payload = {
             user: {
                 id: newUser.insertedId,
             },
@@ -49,9 +49,53 @@ router.post('/register', async (req, res) => {
         const authtoken = jwt.sign(payload, JWT_SECRET);
 
         logger.info('User registered successfully');
-        res.json({authtoken,email});
+        res.json({ authtoken, email });
     } catch (e) {
-         return res.status(500).send('Internal server error');
+        return res.status(500).send('Internal server error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB collection
+        const collection = db.collection("users");
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        //console.log('theUser:', theUser);
+
+        // Task 4: Task 4: Check if the password matches the encrypyted password and send appropriate message on mismatch
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password);
+            //console.log('Result:', result);
+            if (!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            //continue other tasks
+            // Task 5: Fetch user details from database
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            // Task 6: Create JWT authentication if passwords match with user._id as payload
+            const payload = {
+                user: {
+                    id: theUser._id,
+                },
+            };
+    
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            res.json({ authtoken, userName, userEmail });
+        }else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Task 7: Send appropriate message if user not found
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+
     }
 });
 
